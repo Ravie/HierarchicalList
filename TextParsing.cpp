d06b3226dfe014d6adc145262f276458f778dd61
Kirill <ravijkee@yandex.ru>
2016-05-19 22:08:24 +0300

    Init commit

---
+++ TextParsing.cpp
@@ -0,0 +1,12 @@
+#include "HierarchicalList.h"
+TTextMem TLink::TextMem;
+
+void main()
+{
+	TLink::InitMem(100);
+	TText txt;
+	txt.Load("text.txt");
+	TLink::PrintFree();
+	TLink::MemClean(txt);
+	TLink::PrintFree();
+}
